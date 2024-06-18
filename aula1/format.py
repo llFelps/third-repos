1ba9703743b11beb812b409062da7b2ee63ddc4e
llFelps <ll.o.pv.yuzx@gmail.com>
2024-06-17 21:40:56 -0300

    Atividades de format

--- aula1/format.py
+++ aula1/format.py
@@ -4,4 +4,4 @@ mes= "Janeiro"
 valor: int = 500
 desconto: int = 10
 cupom = "PAULAÉ10"
-print("Olá, {}. Em {} você realizou uma compra no valor de R$ {} e ganhou um desconto de {} em sua próxima compra. Use o cumpom {}")format.(nome , mes , valor , desconto , cupom)
+print("Olá, {}. Em {} você realizou uma compra no valor de R$ {} e ganhou um desconto de {} em sua próxima compra. Use o cumpom {}")(nome , mes , valor , desconto , cupom)
