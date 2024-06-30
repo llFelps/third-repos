4466d82185f85067bfa0cf917ff370e1fd55e779
llFelps <ll.o.py.yuzx@gmail.com>
2024-06-30 15:05:30 -0300

    Aula: For, While

---
+++ aula6_repeticao_iteracao/for_while.py
@@ -0,0 +1,29 @@
+##exemplos de estruturas
+# for
+    
+    #for n in range (1, 11):
+    #    print(n)
+    #
+
+##
+# while
+
+    #count = 1
+    # while count <= 10:
+    #    print(count)
+    #    count += 1
+
+    # while not
+    #n_secret = 12
+    #acertou = False
+    #while not acertou:
+    #    palpite = int(input("digite numero de 1 a 100: "))
+    #    if palpite == n_secret:
+    #        print("Acertou miseravi!")
+    #        acertou = True
+    #    elif palpite < n_secret:
+    #        print("Mais")
+    #    else:
+    #        print("Menos")
+    #print("Fin")
+    #
