aab82553d8bffbab53c6e68d66c46d890ad3e4c3
llFelps <ll.o.pv.yuzx@gmail.com>
2024-07-01 20:11:10 -0300

    01/07²

--- aula6_repeticao_iteracao/cep_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-cep = input("digite o cep: ")
-
-response = requests.get('https://viacep.com.br/ws/11630384/json/')
-print("status code:", response.status_code)
-
-data = response.json()
-print("dados:", data['logradouro'])
