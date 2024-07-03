acbd34c8bf5d4857300a0c95c36420e80b0ad97f
llFelps <ll.o.pv.yuzx@gmail.com>
2024-07-03 20:31:08 -0300

    v.env.

---
+++ aula9_massa_teste/dataframe/aula9.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Criando um DataFrame a partir de um dicionário
+data = {
+    'Nome': ['Alice', 'Bob', 'Charlie'],
+    'Idade': [25, 30, 35],
+    'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte']
+}
+
+df = pd.DataFrame(data)
+
+print(df)
